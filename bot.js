4147a6e0d0cb8fe35f2b10ec83131f104f2f259b
Mohlol <34419076+Mohlol@users.noreply.github.com>
2017-12-10 17:26:38 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,21 @@
+/*
+ * Discord Bot Builder Bot
+ * Version 1.2.0
+ * Robert Borghese
+ */
+
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+
+if(!process.send) {
+
+Files.initStandalone();
+
+} else {
+
+process.on('message', function(content) {
+	Files.initBotTest(content);
+});
+	
+bot.on('ready', () => {
+  bot.user.setGame('Making drinks::tropical_drink:|dhelp')
+})
